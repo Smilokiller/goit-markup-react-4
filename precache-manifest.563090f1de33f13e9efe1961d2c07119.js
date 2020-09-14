08b7e016776249e9b34fad3852af72590b765733
Tofik <smilokiller@mail.ru>
2020-09-14 12:11:45 +0300

    Updates

---
+++ precache-manifest.563090f1de33f13e9efe1961d2c07119.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87af1af4ce83c0b2322ed80891356a6a",
+    "url": "/goit-markup-react-4/index.html"
+  },
+  {
+    "revision": "06b9b1fb8c167576ad18",
+    "url": "/goit-markup-react-4/static/css/main.07e648a4.chunk.css"
+  },
+  {
+    "revision": "bb579f901ddc2d9bae14",
+    "url": "/goit-markup-react-4/static/js/0.1f3dd08a.chunk.js"
+  },
+  {
+    "revision": "c0ddfe7e4c0d7cfa46c2",
+    "url": "/goit-markup-react-4/static/js/3.eddbbbb7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/goit-markup-react-4/static/js/3.eddbbbb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4527e525e5a238cae231",
+    "url": "/goit-markup-react-4/static/js/4.c6f8cf94.chunk.js"
+  },
+  {
+    "revision": "ba6b852e94e0939f2850",
+    "url": "/goit-markup-react-4/static/js/5.f4384532.chunk.js"
+  },
+  {
+    "revision": "e896950df954ab8d81dc",
+    "url": "/goit-markup-react-4/static/js/6.3d72e39c.chunk.js"
+  },
+  {
+    "revision": "06b9b1fb8c167576ad18",
+    "url": "/goit-markup-react-4/static/js/main.a363645f.chunk.js"
+  },
+  {
+    "revision": "58e490c2b7eccc7e6b32",
+    "url": "/goit-markup-react-4/static/js/runtime-main.95ca277d.js"
+  }
+]);
---
+++ static/js/5.f4384532.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpmy-app"]=this["webpackJsonpmy-app"]||[]).push([[5],{133:function(n,e,t){"use strict";t.r(e);var a=t(0),p=t.n(a);e.default=function(n){var e=n.genres;return(void 0===e?["null","null"]:e).map((function(n){return p.a.createElement("p",{key:n.id,style:{marginRight:10}},n.name)}))}}}]);
+//# sourceMappingURL=5.f4384532.chunk.js.map
